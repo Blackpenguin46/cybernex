824d3f4b51de4da227ae3074b689a7e975fd66fa
Sam oakes <samoakescyber@proton.me>
2025-02-24 19:34:50 -0500

    update

--- cybernex/app/api/create-checkout-session/route.ts
+++ cybernex/app/api/create-checkout-session/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2022-11-15',
 })
 
 const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN || 'http://localhost:3000'
